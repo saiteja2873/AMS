9e192c49fb42b73a5b5f6499f8d62ecc2b536562
saiteja2873 <pasamsaiteja2020@gmail.com>
2024-10-15 22:30:36 +0530

    connect to mongodb using prisma

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,10 +1,8 @@
 import { Hono } from "hono";
-
-
+import prisma from '../dbSeed'
 
 const app = new Hono();
 
 app.get("/user", (c) => {
-    
     return c.json({ message: "Hello from the user route!" });
 });
