85c24e31f440714adc89570e87deb569fa6ddae9
saiteja2873 <pasamsaiteja2020@gmail.com>
2024-10-15 22:39:28 +0530

    add routes to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import NavBarComponent from './navBarComponent'
 
 function App() {
-  let p: number;
-  p = 0;
   return (
-    <div className='text-blue-400'>
-      <h1 className='bg-blue-100'>My React and TypeScript App!</h1>
-      <p>Here is a paragraph with a value: {p}</p>
-    </div>
+    <>
+    <NavBarComponent/>
+    <Routes>
+      <Route path='/' element={<>hello</>}/>
+      <Route path='/login' element={<>login</>}/>
+    </Routes>
+    </>
   )
 }
 
