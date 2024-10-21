314a2e03da007c9b0bb1670149fa78f7675f2ce3
saiteja2873 <pasamsaiteja2020@gmail.com>
2024-10-21 22:50:05 +0530

    Add CRUD operations for crop route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { Jwt } from 'jsonwebtoken'
+import cropRoute from './routes/crop'
 
 
 const app = new Hono()
 
+app.route('/crop', cropRoute)
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
---
+++ backend/src/routes/crop.ts
@@ -0,0 +1,98 @@
+import { Hono } from "hono";
+import prisma from "../dbSeed";
+
+const app = new Hono();
+
+app.get('/', async (c) => {
+    const { crop, state, district} = await c.req.json();
+    try {
+        const targetCrop = await prisma.crop.findFirst({
+            where: {
+                crop,
+                state,
+                district
+            }
+        })
+        return c.json({crop: targetCrop}, 200)
+    } catch (err) {
+        return c.json({error: "Something went wrong"}, 401);
+    }
+})
+
+//TODO: Check Authorization (allow only for admins)
+app.post('/', async(c) => {
+    const {crop, state, district, msp, marketPrice, seedsCost, irrigationCost, fertilizerCost, labourCost } =  await c.req.json();
+    try {
+        const newCrop =await prisma.crop.create({
+            data: {
+                crop,
+                state,
+                district,
+                msp,
+                marketPrice
+            }
+        });
+
+        const newCropCost = await prisma.costTracking.create({
+            data: {
+                cropId: newCrop.id,
+                seedsCost,
+                irrigationCost,
+                fertilizerCost,
+                labourCost
+            }
+        }) 
+        return c.json({crop: newCrop, cost: newCropCost}, 200);
+    } catch (err) {
+        return c.json({error: "Something went wrong"}, 401);
+    }
+})
+
+app.put('/', async (c) => {
+    const {crop, state, district, msp, marketPrice, seedsCost, irrigationCost, fertilizerCost, labourCost } =  await c.req.json();
+    try {
+        const updatedCrop = await prisma.crop.update({
+            where: {
+                id: crop.id
+            },
+            data: {
+                crop,
+                state,
+                district,
+                msp,
+                marketPrice
+            }
+        });
+
+        const updatedCropCost = await prisma.costTracking.update({
+            where: {
+                cropId: updatedCrop.id
+            },
+            data: {
+                seedsCost,
+                irrigationCost,
+                fertilizerCost,
+                labourCost
+            }
+        });
+        return c.json({crop: updatedCrop, cost: updatedCropCost}, 200)
+    } catch (error) {
+        return c.json({error: "Something went wrong"}, 401)
+    }
+});
+
+app.delete('/', async (c) => {
+    const {id} = await c.req.json();
+    try {
+        const deletedCrop = await prisma.crop.delete({
+            where: {
+                id
+            }
+        })
+        return c.json({deletedCrop}, 200)
+    } catch (error) {
+        return c.json({error: "Something went wrong"}, 401)
+    }
+})
+
+export default app;
